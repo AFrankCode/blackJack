80d0ab0fe6d2cc6b505c7c31f85c162c3a3faca8
AFrankCode <139520217+AFrankCode@users.noreply.github.com>
2023-12-13 22:25:06 -0600

    2nd commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 10
 let secondCard = 4
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive=true
@@ -27,15 +28,39 @@ function renderGame() {
     }
     messageEl.textContent = message
     sumEl.textContent = "Sum: " + sum
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
 }
 function newCard() {
   console.log("Drawing a new card from the deck!")
   let card = 7
   sum += card
+  cards.push(card)
   renderGame()
 
 }
 
 
 console.log (message)
+
+//let myLearning = [
+  //  "Can you believe its only been a few months",
+   // "Here the code to what I've made so far",
+   // "Take a look at my porfolio showing examples of javascript/html"
+
+//]
+//let mySelf = [
+  //  "I am a complex being",
+  //  29,
+   // iLikePizza = true,
+//]
+//for (let i=10; count < 101; i += 10 ) {
+   // console.log(i)
+//}
+
+// function Car(make, model, wheels) {
+//     this.make=make;
+//     this.model=model;
+//     this.wheels=wheels;
+// }
+
+// let diablo = new Car('Lambo', 'Diablo', '4')
